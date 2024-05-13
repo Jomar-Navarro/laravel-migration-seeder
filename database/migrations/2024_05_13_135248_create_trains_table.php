b77c9eb530c8824b81fd02e6f293407b67c82d62
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-13 16:04:22 +0200

    [add] slug

--- database/migrations/2024_05_13_135248_create_trains_table.php
+++ database/migrations/2024_05_13_135248_create_trains_table.php
@@ -14,14 +14,15 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company');
+            $table->string('slug', 60)->unique();
             $table->string('departure_station');
             $table->string('arrival_station');
             $table->time('departure_time');
             $table->time('arrival_time');
             $table->string('train_code');
             $table->tinyInteger('carriages_number');
-            $table->boolean('on_time');
-            $table->boolean('cancelled');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
