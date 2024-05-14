b762d4ff9d7d4e50721ec53db8f7fcc5f21ee0c6
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-14 16:20:42 +0200

    [add] table seeder populated

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -16,17 +17,37 @@ class TrainsTableSeeder extends Seeder
     {
         for($i = 0; $i < 87; $i++){
             $new_train = new Train();
-            $new_train->company;
-            $new_train->slug;
-            $new_train->company;
-            $new_train->company;
-            $new_train->company;
-            $new_train->company;
-            $new_train->company;
-            $new_train->company;
-            $new_train->company;
-            $new_train->company;
-            $new_train->company;
+            $new_train->company = $faker->words(2, true);
+            $new_train->slug = $this->generateSlug($new_train->company);
+            $new_train->departure_station = $faker->words(2, true);
+            $new_train->arrival_station = $faker->words(2, true);
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_code = $faker->ean13();
+            $new_train->ticket_price = $faker->numberBetween(1, 10000);
+            $new_train->carriages_number = $faker->numberBetween(1, 10);
+
+            $new_train->save();
+            // dump($new_train);
         }
     }
+
+    private function generateSlug($string){
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+
+
+        $exists = Train::where('slug', $slug)->first();
+        $c = 1;
+
+        while($exists){
+            $slug = $original_slug . '-' . $c;
+            $exists = Train::where('slug', $slug)->first();
+            $c++;
+
+            return $slug;
+        }
+    }
+
+
 }
