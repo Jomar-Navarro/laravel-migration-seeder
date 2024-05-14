f0c2fbb7edaa6cc333739b462f92130245dcb636
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-14 16:35:49 +0200

    [add] header navbar

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/nuova-pagina', [PageController::class, 'nuovaPagina'])->name('nuova-pagina');
+Route::get('/Trains', [PageController::class, 'Trains'])->name('Trains');
 
 
 
